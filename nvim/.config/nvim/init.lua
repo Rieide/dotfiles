5229b5277dcb27a2b6e9d292c3e8af70b3ca5424
Rieide <riei.de@outlook.com>
2026-07-04 11:59:57 -0700

    nvim: add split keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -224,6 +224,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>\\', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = 'Split window horizontally' })
 
 -- Fast buffer switching lives in custom.plugins.bufferline, where number keys
 -- follow the visible bufferline order rather than Vim's internal buffer ids.
