dd5d9a8cb08ea9dd5afcae87feb7a3f39b639448
Rieide <riei.de@outlook.com>
2026-09-01 17:44:57 +0800

    nvim: recover incomplete lazy.nvim checkout

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -299,10 +299,20 @@ vim.api.nvim_create_autocmd({ 'VimResized', 'WinNew', 'WinClosed' }, {
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+local lazy_init = lazypath .. '/lua/lazy/init.lua'
+if not (vim.uv or vim.loop).fs_stat(lazy_init) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+  local command
+
+  if (vim.uv or vim.loop).fs_stat(lazypath .. '/.git') then
+    -- Recover an interrupted or otherwise empty checkout.
+    command = { 'git', '-C', lazypath, 'reset', '--hard', 'HEAD' }
+  else
+    command = { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  end
+
+  local out = vim.fn.system(command)
+  if vim.v.shell_error ~= 0 or not (vim.uv or vim.loop).fs_stat(lazy_init) then error('Error installing lazy.nvim:\n' .. out) end
 end
 
 ---@type vim.Option
